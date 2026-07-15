f7830bd7e5014b37e0e0dce10cb5bc10a442cda6
CarlosRAVA <carlosbrv251023@gmail.com>
2026-07-15 07:53:58 -0600

    feat: agregar concurrently y script dev en la raiz para levantar todo el proyecto

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,7 +1,13 @@
+import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
+
+console.log(process.env.DATABASE_URL);
 
 export default defineConfig({
   datasource: {
     url: process.env.DATABASE_URL,
+  },
+  migrations: {
+    seed: 'ts-node prisma/seed.ts',
   },
 });
